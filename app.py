1def819fd0ddee87e0738b99f2ab4ee92771b3f2
Eazi <chisome.azi@gmail.com>
2026-05-06 14:28:34 +0100

    updated project

--- app.py
+++ app.py
@@ -71,4 +71,6 @@ def fetch_ensembl():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5001)
+    import os
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=False, host='0.0.0.0', port=port)
